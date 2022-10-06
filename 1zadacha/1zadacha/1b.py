dbee6b8c824bb6b45357571f1a24c14eb1dcf667
85dobrovolsky85 <dobrovolsky.evgey@yandex.ru>
2022-10-06 19:41:49 +0300

    1b.py

--- 1zadacha/1zadacha/1b.py
+++ 1zadacha/1zadacha/1b.py
@@ -1,14 +1,7 @@
-# bool
-# print('X Y Z')
-# for x in 0,1:
-#     for y in 0,1:
-#         for z in 0,1:
-#             if not(x or y or z) = not x and not y and not z:
-#                 print(x,y,z,)
-print('y', 'x', 'z', 'F') #Напечатаем заголовки таблицы
-for y in range(2): #Берём все переменные и меняем их в циклах '0' и '1'
-	for x in range(2):
-		for z in range(2):
-			for w in range(2):
-				F = not (x or y or z) = not x and not y and not z #Записываем функцию
-				print(x, y, z, F) #Выводим результат
+#задача 2. Напишите программу для. 
+# проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
+# для всех значений предикат.
+from itertools import product
+  
+for x, y, z in product([0, 1], repeat=3): 
+    print(not(x or y or z) , not x and not y and not z)
